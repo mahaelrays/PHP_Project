dc6f7678a5d45fb088ec8b9961632b7b8a70758f
m-foda <m.foda91@gmail.com>
2017-02-10 19:40:26 +0200

    edited database

--- Control/electro1.sql
+++ Control/electro1.sql
@@ -1,108 +1,220 @@
--- phpMyAdmin SQL Dump
--- version 4.5.4.1deb2ubuntu2
--- http://www.phpmyadmin.net
+-- MySQL dump 10.13  Distrib 5.7.17, for Linux (x86_64)
 --
--- Host: localhost
--- Generation Time: Feb 09, 2017 at 11:17 PM
--- Server version: 5.7.17-0ubuntu0.16.04.1
--- PHP Version: 7.0.13-0ubuntu0.16.04.1
-
-SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
-SET time_zone = "+00:00";
-
+-- Host: localhost    Database: phpproject
+-- ------------------------------------------------------
+-- Server version	5.7.17-0ubuntu0.16.04.1
 
 /*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
 /*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
 /*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
-/*!40101 SET NAMES utf8mb4 */;
+/*!40101 SET NAMES utf8 */;
+/*!40103 SET @OLD_TIME_ZONE=@@TIME_ZONE */;
+/*!40103 SET TIME_ZONE='+00:00' */;
+/*!40014 SET @OLD_UNIQUE_CHECKS=@@UNIQUE_CHECKS, UNIQUE_CHECKS=0 */;
+/*!40014 SET @OLD_FOREIGN_KEY_CHECKS=@@FOREIGN_KEY_CHECKS, FOREIGN_KEY_CHECKS=0 */;
+/*!40101 SET @OLD_SQL_MODE=@@SQL_MODE, SQL_MODE='NO_AUTO_VALUE_ON_ZERO' */;
+/*!40111 SET @OLD_SQL_NOTES=@@SQL_NOTES, SQL_NOTES=0 */;
 
 --
--- Database: `electro`
+-- Table structure for table `admins`
 --
 
--- --------------------------------------------------------
+DROP TABLE IF EXISTS `admins`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!40101 SET character_set_client = utf8 */;
+CREATE TABLE `admins` (
+  `admin_id` int(10) unsigned NOT NULL,
+  `admin_name` varchar(45) NOT NULL,
+  `admin_email` varchar(45) NOT NULL,
+  `admin_pass` varchar(45) NOT NULL,
+  PRIMARY KEY (`admin_id`),
+  UNIQUE KEY `admin_email_UNIQUE` (`admin_email`)
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `admins`
+--
+
+LOCK TABLES `admins` WRITE;
+/*!40000 ALTER TABLE `admins` DISABLE KEYS */;
+/*!40000 ALTER TABLE `admins` ENABLE KEYS */;
+UNLOCK TABLES;
 
 --
 -- Table structure for table `category`
 --
 
+DROP TABLE IF EXISTS `category`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!40101 SET character_set_client = utf8 */;
 CREATE TABLE `category` (
-  `category_id` int(10) UNSIGNED NOT NULL,
+  `category_id` int(10) unsigned NOT NULL AUTO_INCREMENT,
   `category_name` varchar(20) NOT NULL,
-  `parent_cat_id` int(11) DEFAULT NULL
+  `parent_cat_id` int(11) unsigned DEFAULT NULL,
+  PRIMARY KEY (`category_id`)
 ) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+/*!40101 SET character_set_client = @saved_cs_client */;
 
--- --------------------------------------------------------
+--
+-- Dumping data for table `category`
+--
+
+LOCK TABLES `category` WRITE;
+/*!40000 ALTER TABLE `category` DISABLE KEYS */;
+/*!40000 ALTER TABLE `category` ENABLE KEYS */;
+UNLOCK TABLES;
 
 --
 -- Table structure for table `interests`
 --
 
+DROP TABLE IF EXISTS `interests`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!40101 SET character_set_client = utf8 */;
 CREATE TABLE `interests` (
-  `userinter_id` int(10) UNSIGNED NOT NULL,
-  `catinter_id` int(10) UNSIGNED NOT NULL
+  `userinter_id` int(10) unsigned NOT NULL,
+  `catinter_id` int(10) unsigned NOT NULL,
+  PRIMARY KEY (`userinter_id`,`catinter_id`),
+  KEY `userinter_id` (`userinter_id`),
+  KEY `catinter_id` (`catinter_id`),
+  CONSTRAINT `interests_ibfk_1` FOREIGN KEY (`userinter_id`) REFERENCES `users` (`user_id`),
+  CONSTRAINT `interests_ibfk_2` FOREIGN KEY (`catinter_id`) REFERENCES `category` (`category_id`)
 ) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+/*!40101 SET character_set_client = @saved_cs_client */;
 
--- --------------------------------------------------------
+--
+-- Dumping data for table `interests`
+--
+
+LOCK TABLES `interests` WRITE;
+/*!40000 ALTER TABLE `interests` DISABLE KEYS */;
+/*!40000 ALTER TABLE `interests` ENABLE KEYS */;
+UNLOCK TABLES;
 
 --
 -- Table structure for table `log`
 --
 
+DROP TABLE IF EXISTS `log`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!40101 SET character_set_client = utf8 */;
 CREATE TABLE `log` (
-  `orderlog_id` int(10) UNSIGNED NOT NULL,
-  `userlog_id` int(10) UNSIGNED NOT NULL,
-  `order_date` date NOT NULL
+  `orderlog_id` int(10) unsigned NOT NULL,
+  `userlog_id` int(10) unsigned NOT NULL,
+  `order_date` date NOT NULL,
+  PRIMARY KEY (`orderlog_id`),
+  KEY `userlog_id` (`userlog_id`),
+  KEY `orderlog_id` (`orderlog_id`),
+  CONSTRAINT `log_ibfk_1` FOREIGN KEY (`userlog_id`) REFERENCES `users` (`user_id`),
+  CONSTRAINT `log_ibfk_2` FOREIGN KEY (`orderlog_id`) REFERENCES `orders` (`order_id`)
 ) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+/*!40101 SET character_set_client = @saved_cs_client */;
 
--- --------------------------------------------------------
+--
+-- Dumping data for table `log`
+--
+
+LOCK TABLES `log` WRITE;
+/*!40000 ALTER TABLE `log` DISABLE KEYS */;
+/*!40000 ALTER TABLE `log` ENABLE KEYS */;
+UNLOCK TABLES;
 
 --
 -- Table structure for table `orders`
 --
 
+DROP TABLE IF EXISTS `orders`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!40101 SET character_set_client = utf8 */;
 CREATE TABLE `orders` (
-  `order_id` int(10) UNSIGNED NOT NULL,
-  `prodorder_id` int(10) UNSIGNED NOT NULL,
-  `orderquantity` int(11) DEFAULT NULL
+  `order_id` int(10) unsigned NOT NULL,
+  `prodorder_id` int(10) unsigned NOT NULL,
+  `orderquantity` int(11) DEFAULT NULL,
+  PRIMARY KEY (`order_id`,`prodorder_id`),
+  KEY `order_id` (`order_id`),
+  KEY `fk_orders_1_idx` (`prodorder_id`),
+  CONSTRAINT `fk_orders_1` FOREIGN KEY (`prodorder_id`) REFERENCES `products` (`product_id`) ON DELETE NO ACTION ON UPDATE NO ACTION
 ) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+/*!40101 SET character_set_client = @saved_cs_client */;
 
--- --------------------------------------------------------
+--
+-- Dumping data for table `orders`
+--
+
+LOCK TABLES `orders` WRITE;
+/*!40000 ALTER TABLE `orders` DISABLE KEYS */;
+/*!40000 ALTER TABLE `orders` ENABLE KEYS */;
+UNLOCK TABLES;
 
 --
 -- Table structure for table `products`
 --
 
+DROP TABLE IF EXISTS `products`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!40101 SET character_set_client = utf8 */;
 CREATE TABLE `products` (
-  `product_id` int(10) UNSIGNED NOT NULL,
-  `product_name` varchar(40) NOT NULL,
-  `pic_path` varchar(100) NOT NULL,
+  `product_id` int(10) unsigned NOT NULL,
+  `product_name` varchar(45) NOT NULL,
+  `price` int(11) NOT NULL,
+  `pic_path` varchar(100) DEFAULT NULL,
   `quantity` int(11) NOT NULL,
-  `brand` varchar(20) NOT NULL,
+  `brand` varchar(45) NOT NULL,
   `description` varchar(100) DEFAULT NULL,
-  `subcat_id` int(10) UNSIGNED NOT NULL
+  `subcat_id` int(10) unsigned NOT NULL,
+  PRIMARY KEY (`product_id`),
+  KEY `fk_products_1_idx` (`subcat_id`),
+  CONSTRAINT `fk_products_1` FOREIGN KEY (`subcat_id`) REFERENCES `category` (`category_id`) ON DELETE NO ACTION ON UPDATE NO ACTION
 ) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+/*!40101 SET character_set_client = @saved_cs_client */;
 
--- --------------------------------------------------------
+--
+-- Dumping data for table `products`
+--
+
+LOCK TABLES `products` WRITE;
+/*!40000 ALTER TABLE `products` DISABLE KEYS */;
+/*!40000 ALTER TABLE `products` ENABLE KEYS */;
+UNLOCK TABLES;
 
 --
 -- Table structure for table `shopping_cart`
 --
 
+DROP TABLE IF EXISTS `shopping_cart`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!40101 SET character_set_client = utf8 */;
 CREATE TABLE `shopping_cart` (
-  `usercart_id` int(10) UNSIGNED NOT NULL,
-  `prodcart_id` int(10) UNSIGNED NOT NULL,
-  `order_quantity` int(11) DEFAULT NULL
+  `usercart_id` int(10) unsigned NOT NULL,
+  `prodcart_id` int(10) unsigned NOT NULL,
+  `order_quantity` int(11) DEFAULT NULL,
+  PRIMARY KEY (`usercart_id`,`prodcart_id`),
+  KEY `usercart_id` (`usercart_id`),
+  KEY `fk_shopping_cart_1_idx` (`prodcart_id`),
+  CONSTRAINT `fk_shopping_cart_1` FOREIGN KEY (`prodcart_id`) REFERENCES `products` (`product_id`) ON DELETE NO ACTION ON UPDATE NO ACTION,
+  CONSTRAINT `shopping_cart_ibfk_1` FOREIGN KEY (`usercart_id`) REFERENCES `users` (`user_id`)
 ) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+/*!40101 SET character_set_client = @saved_cs_client */;
 
--- --------------------------------------------------------
+--
+-- Dumping data for table `shopping_cart`
+--
+
+LOCK TABLES `shopping_cart` WRITE;
+/*!40000 ALTER TABLE `shopping_cart` DISABLE KEYS */;
+/*!40000 ALTER TABLE `shopping_cart` ENABLE KEYS */;
+UNLOCK TABLES;
 
 --
 -- Table structure for table `users`
 --
 
+DROP TABLE IF EXISTS `users`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!40101 SET character_set_client = utf8 */;
 CREATE TABLE `users` (
-  `user_id` int(10) UNSIGNED NOT NULL,
+  `user_id` int(10) unsigned NOT NULL,
   `user_name` varchar(30) NOT NULL,
   `birthday` date NOT NULL,
   `job` varchar(40) NOT NULL,
@@ -110,123 +222,31 @@ CREATE TABLE `users` (
   `password` varchar(20) NOT NULL,
   `credit` int(11) NOT NULL,
   `address` varchar(100) NOT NULL,
-  `delete_admin_id` int(11) DEFAULT NULL,
-  `delete_reason` varchar(100) NOT NULL
+  `delete_admin_id` int(11) unsigned DEFAULT NULL,
+  `delete_reason` varchar(100) NOT NULL,
+  PRIMARY KEY (`user_id`),
+  UNIQUE KEY `email` (`email`),
+  KEY `fk_users_1_idx` (`delete_admin_id`),
+  CONSTRAINT `fk_users_1` FOREIGN KEY (`delete_admin_id`) REFERENCES `admins` (`admin_id`) ON DELETE NO ACTION ON UPDATE NO ACTION
 ) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+/*!40101 SET character_set_client = @saved_cs_client */;
 
 --
--- Indexes for dumped tables
+-- Dumping data for table `users`
 --
 
---
--- Indexes for table `category`
---
-ALTER TABLE `category`
-  ADD PRIMARY KEY (`category_id`);
+LOCK TABLES `users` WRITE;
+/*!40000 ALTER TABLE `users` DISABLE KEYS */;
+/*!40000 ALTER TABLE `users` ENABLE KEYS */;
+UNLOCK TABLES;
+/*!40103 SET TIME_ZONE=@OLD_TIME_ZONE */;
 
---
--- Indexes for table `interests`
---
-ALTER TABLE `interests`
-  ADD PRIMARY KEY (`userinter_id`,`catinter_id`),
-  ADD KEY `userinter_id` (`userinter_id`),
-  ADD KEY `catinter_id` (`catinter_id`);
-
---
--- Indexes for table `log`
---
-ALTER TABLE `log`
-  ADD PRIMARY KEY (`orderlog_id`),
-  ADD KEY `userlog_id` (`userlog_id`),
-  ADD KEY `orderlog_id` (`orderlog_id`);
-
---
--- Indexes for table `orders`
---
-ALTER TABLE `orders`
-  ADD PRIMARY KEY (`order_id`,`prodorder_id`),
-  ADD KEY `order_id` (`order_id`),
-  ADD KEY `prodorder_id` (`prodorder_id`);
-
---
--- Indexes for table `products`
---
-ALTER TABLE `products`
-  ADD PRIMARY KEY (`product_id`),
-  ADD UNIQUE KEY `cat_id_12` (`subcat_id`),
-  ADD KEY `cat_id` (`subcat_id`),
-  ADD KEY `cat_id_2` (`subcat_id`),
-  ADD KEY `cat_id_3` (`subcat_id`),
-  ADD KEY `cat_id_4` (`subcat_id`),
-  ADD KEY `cat_id_5` (`subcat_id`),
-  ADD KEY `cat_id_6` (`subcat_id`),
-  ADD KEY `cat_id_7` (`subcat_id`),
-  ADD KEY `cat_id_8` (`subcat_id`),
-  ADD KEY `cat_id_9` (`subcat_id`),
-  ADD KEY `cat_id_10` (`subcat_id`),
-  ADD KEY `cat_id_11` (`subcat_id`);
-
---
--- Indexes for table `shopping_cart`
---
-ALTER TABLE `shopping_cart`
-  ADD PRIMARY KEY (`usercart_id`,`prodcart_id`),
-  ADD KEY `usercart_id` (`usercart_id`),
-  ADD KEY `prodcart_id` (`prodcart_id`);
-
---
--- Indexes for table `users`
---
-ALTER TABLE `users`
-  ADD PRIMARY KEY (`user_id`),
-  ADD UNIQUE KEY `email` (`email`);
-
---
--- AUTO_INCREMENT for dumped tables
---
-
---
--- AUTO_INCREMENT for table `category`
---
-ALTER TABLE `category`
-  MODIFY `category_id` int(10) UNSIGNED NOT NULL AUTO_INCREMENT;
---
--- Constraints for dumped tables
---
-
---
--- Constraints for table `interests`
---
-ALTER TABLE `interests`
-  ADD CONSTRAINT `interests_ibfk_1` FOREIGN KEY (`userinter_id`) REFERENCES `users` (`user_id`),
-  ADD CONSTRAINT `interests_ibfk_2` FOREIGN KEY (`catinter_id`) REFERENCES `category` (`category_id`);
-
---
--- Constraints for table `log`
---
-ALTER TABLE `log`
-  ADD CONSTRAINT `log_ibfk_1` FOREIGN KEY (`userlog_id`) REFERENCES `users` (`user_id`),
-  ADD CONSTRAINT `log_ibfk_2` FOREIGN KEY (`orderlog_id`) REFERENCES `orders` (`order_id`);
-
---
--- Constraints for table `orders`
---
-ALTER TABLE `orders`
-  ADD CONSTRAINT `orders_ibfk_1` FOREIGN KEY (`prodorder_id`) REFERENCES `products` (`product_id`);
-
---
--- Constraints for table `products`
---
-ALTER TABLE `products`
-  ADD CONSTRAINT `products_ibfk_1` FOREIGN KEY (`subcat_id`) REFERENCES `category` (`category_id`);
-
---
--- Constraints for table `shopping_cart`
---
-ALTER TABLE `shopping_cart`
-  ADD CONSTRAINT `shopping_cart_ibfk_1` FOREIGN KEY (`usercart_id`) REFERENCES `users` (`user_id`),
-  ADD CONSTRAINT `shopping_cart_ibfk_2` FOREIGN KEY (`prodcart_id`) REFERENCES `products` (`product_id`);
-
+/*!40101 SET SQL_MODE=@OLD_SQL_MODE */;
+/*!40014 SET FOREIGN_KEY_CHECKS=@OLD_FOREIGN_KEY_CHECKS */;
+/*!40014 SET UNIQUE_CHECKS=@OLD_UNIQUE_CHECKS */;
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
 /*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
 /*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
+/*!40111 SET SQL_NOTES=@OLD_SQL_NOTES */;
+
+-- Dump completed on 2017-02-10 19:31:11
